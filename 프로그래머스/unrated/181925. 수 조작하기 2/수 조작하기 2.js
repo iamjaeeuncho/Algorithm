915a378d9865113f88d1c82eb1ca93b46cafe7a6
JaeeunCho <46369038+iamjaeeuncho@users.noreply.github.com>
2023-11-26 16:45:45 +0900

    [level unrated] Title: 수 조작하기 2, Time: 7.28 ms, Memory: 41.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,17 @@
+function solution(numLog) {
+    var answer = '';
+    // return answer;
+    
+    for (let i = 0; i < numLog.length; i++) {
+        if ((numLog[i-1] || 0) + 1 === numLog[i]) {
+            answer += 'w'
+        } else if ((numLog[i-1] || 0) - 1 === numLog[i]) {
+            answer += 's'
+        } else if ((numLog[i-1] || 0) + 10 === numLog[i]) {
+            answer += 'd'
+        } else if ((numLog[i-1] || 0) - 10 === numLog[i]) {
+            answer += 'a'
+        }
+    }
+    return answer;
+}
